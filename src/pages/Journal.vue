2e91a64bf41c7d904e9f9dc869d174060e1f34df
jwxyan <battlelab@qq.com>
2021-03-02 00:38:49 +0800

    Update Journal Page Template

--- src/pages/Journal.vue
+++ src/pages/Journal.vue
@@ -1,5 +1,27 @@
 <template>
-  <Layout></Layout>
+  <Layout>
+    <div data-v-da896d1e="" class="container">
+      <div data-v-da896d1e="" class="journal-hero">
+        <h1 data-v-da896d1e="" class="journal-header">
+          a wise person once said...
+        </h1>
+      </div>
+    </div>
+    <a
+      data-v-da896d1e=""
+      href="/journal/gridsome-forestry-cms/"
+      class="journal-post"
+      ><div data-v-da896d1e="" class="container journal">
+        <h2 data-v-da896d1e="" class="journal-title">
+          Gridsome with Forestry CMS
+        </h2>
+        <p data-v-da896d1e="" class="journal-excerpt">
+          Get up and running with Forestry CMS, a git-backed headless CMS with a
+          slick editing interface.
+        </p>
+      </div></a
+    >
+  </Layout>
 </template>
 
 <script>
